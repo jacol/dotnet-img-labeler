b8b240e5fc0235add3bc2baf0876c53fbfb043f6
Jacek <jnikitiuk@otrzy.pl>
2021-01-17 21:47:23 +0100

    initial commit with main projects structure

---
+++ src/dotnet-img-labeler/dotnet-img-labeler-console-app/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnet_img_labeler_console_app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ src/dotnet-img-labeler/dotnet-img-labeler-lib/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace dotnet_img_labeler_lib
+{
+    public class Class1
+    {
+    }
+}
